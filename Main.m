52dca36cb0021e40472e7d0bca5c1998d6fbc33f
Adaso <adamsdm1@gmail.com>
2016-11-01 09:43:17 +0100

    Added TODO Comments for initial steps

--- Main.m
+++ Main.m
@@ -2,8 +2,25 @@
 %%%% TNM034(Im) testing %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Read image and convert to double format 
-Im =  im2double(imread('./Testbilder/im1s.jpg'));
+%% Read image and convert to double, grayscale format 
+clear;
+Im = rgb2gray( im2double( imread('./Testbilder/im1s.jpg') ) );
 
-TNM034(Im);
+%% Remove staff lines 
+
+% TODO (Optimal recognition of music symbols, 3.2)
+% "line track height algorithm with the stable path algorithm
+% to remove staff lines"
+
+%% Symbol segmentation (Optimal recognition of music symbols, 3.3)
+% "localizing and isolating the symbols in order to identify them"
+
+% TODO Beam Detection
+
+% TODO Notes, notes with flags and notes open detection
+
+
+
+%%
+imshow(Im);
  
