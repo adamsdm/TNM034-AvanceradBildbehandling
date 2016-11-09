35b33ae611f598f9b4b1387822dd63706d6df8e2
oscko391 <oscko391@student.liu.se>
2016-11-09 17:04:18 +0100

    Added plot for peak values of the different angles

--- findBestRotAngle.m
+++ findBestRotAngle.m
@@ -1,6 +1,7 @@
 function [ A ] = findBestRotAngle( invBW )
-%findBestRotAngle - finds the best rotation angle
-%   invBW = logical image
+%findBestRotAngle - finds the rotation angle which gives the biggest peaks
+%of the histogram
+%   invBW = logical black and white image
 %   A     = the angle which gives best rotation with respect to
 %           max peaks in histogram
 
@@ -10,15 +11,21 @@ function [ A ] = findBestRotAngle( invBW )
 
 A = -1.0;
 maxPeak = 0;
-
+%X = zeros(1,20);
+%count = 1;
 for angle=-1.0:0.1:1.0
    rotIm = imrotate(invBW, angle);      % Rotate the image with angle from -1.0 - 1.0 degrees. 
    peak = max( sum(rotIm(:,:)' ) );   % Calculate the integral of the histogram.
+   %X(count) = peak;
    if(peak > maxPeak)
        maxPeak = peak;
        A = angle;
    end
+   count = count +1;
 end  
+size(X)
+%plot(-1.0:0.1:1.0,X); %uncomment count and X to get a plot for the peak
+%values with corresponding histogram angle.
 
 end
 
